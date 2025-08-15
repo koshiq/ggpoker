6ac51118a701f6589e0eb535f77d4c975a23e4c8
k0shiq <koshiqhossain@gmail.com>
2025-08-14 23:25:18 -0400

    updated

--- p2p/server.go
+++ p2p/server.go
@@ -1,60 +1,44 @@
 package p2p
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"net"
 	"sync"
 )
-
-type Peer struct {
-	conn net.Conn
-}
-
-func (p *Peer) Send(b []byte) error {
-	_, err := p.conn.Write(b)
-	return err
-}
 
 type ServerConfig struct {
 	Version    string
 	ListenAddr string
 }
 
-type Message struct {
-	Payload io.Reader
-	From    net.Addr
-}
-
 type Server struct {
 	ServerConfig
 
-	handler  Handler
-	listener net.Listener
-	mu       sync.RWMutex
-	peers    map[net.Addr]*Peer
-	addPeer  chan *Peer
-	delPeer  chan *Peer
-	msgCh    chan *Message
+	handler   Handler
+	transport *TCPTransport
+	mu        sync.RWMutex
+	peers     map[net.Addr]*Peer
+	addPeer   chan *Peer
+	delPeer   chan *Peer
+	msgCh     chan *Message
 }
 
 func NewServer(cfg ServerConfig) *Server {
-	return &Server{
+
+	s := &Server{
 		handler:      &DefaultHandler{},
 		ServerConfig: cfg,
 		peers:        make(map[net.Addr]*Peer),
 		addPeer:      make(chan *Peer),
 		msgCh:        make(chan *Message),
 	}
+
+	s.transport = NewTCPTransport(cfg.ListenAddr, s.addPeer, s.delPeer)
+	return s
 }
 
 func (s *Server) Start() {
 	go s.loop()
-	if err := s.listen(); err != nil {
-		panic(err)
-	}
-
 	fmt.Printf("game server running on TCP port %s\n", s.ServerConfig.ListenAddr)
 	s.acceptLoop()
 }
@@ -72,50 +56,6 @@ func (s *Server) Connect(addr string) error {
 	s.addPeer <- peer
 
 	return peer.Send([]byte(s.Version))
-
-}
-
-func (s *Server) acceptLoop() {
-	for {
-		conn, err := s.listener.Accept()
-		if err != nil {
-			panic(err)
-		}
-
-		peer := &Peer{
-			conn: conn,
-		}
-
-		s.addPeer <- peer
-
-		peer.Send([]byte(s.Version))
-		go s.handleConn(peer)
-	}
-}
-
-func (s *Server) handleConn(p *Peer) {
-	buf := make([]byte, 1024)
-	for {
-		n, err := p.conn.Read(buf)
-		if err != nil {
-			break
-		}
-
-		s.msgCh <- &Message{
-			From:    p.conn.RemoteAddr(),
-			Payload: bytes.NewReader(buf[:n]),
-		}
-	}
-	s.delPeer <- p
-}
-
-func (s *Server) listen() error {
-	ln, err := net.Listen("tcp", s.ServerConfig.ListenAddr)
-	if err != nil {
-		return err
-	}
-	s.listener = ln
-	return nil
 
 }
 
